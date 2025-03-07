bb7e66bc5d84c04dd12b2d5bc9a7b9663581752a
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-03-07 17:02:39 +0300

    add QuestionTimer component to manage question timeouts in Quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,14 @@
 import {useState} from "react";
-import QUENSTIONS from "../questions.js";
+import QUESTIONS from "../questions.js";
 import quizComplete from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
-    const quizIsComplete = activeQuestionIndex === QUENSTIONS.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
@@ -24,14 +25,15 @@ export default function Quiz() {
         );
     }
 
-    const shuffledAnswers = [...QUENSTIONS[activeQuestionIndex].answers];
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
     shuffledAnswers.sort(() => Math.random() - 0.5);
 
 
     return (
         <div id="quiz">
             <div id="question">
-                <h2>{QUENSTIONS[activeQuestionIndex].text}</h2>
+                <QuestionTimer timeout={10000} onTimeout={()=> handleSelectAnswer(null)} />
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
