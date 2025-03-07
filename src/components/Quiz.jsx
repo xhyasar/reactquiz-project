a8deeec3f6a9a9af8a5826cef463ecb2476f4a5c
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-03-07 15:55:08 +0300

    shuffle answers for current question in Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,7 +4,10 @@ import QUENSTIONS from "../questions.js";
 export default function Quiz() {
 
 const [userAnswers, setUserAnswers] = useState([]);
+
 const activeQuestionIndex = userAnswers.length;
+const shuffledAnswers = [...QUENSTIONS[activeQuestionIndex].answers];
+shuffledAnswers.sort(() => Math.random() - 0.5);
 
 function handleSelectAnswer(selectedAnswer){
     setUserAnswers((prevUserAnswers) => {
@@ -17,7 +20,7 @@ function handleSelectAnswer(selectedAnswer){
             <div id="question">
                 <h2>{QUENSTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUENSTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
